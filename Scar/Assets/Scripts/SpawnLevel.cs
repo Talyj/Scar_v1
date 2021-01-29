cfced561c5eb9abcdbb9d22a1b615aff8b65def0
Ines <benaziz.ines@gmail.com>
2021-01-29 18:21:16 +0100

    generateur random level fonctionnel

---
+++ Scar/Assets/Scripts/SpawnLevel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnLevel : MonoBehaviour
+{
+    public GameObject[] rooms;
+    public GameObject spawnPoint;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Instantiate( rooms[Random.Range(0, rooms.Length)], spawnPoint.transform.position, spawnPoint.transform.rotation);
+        }
+    }
+    
+}
