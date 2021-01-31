88634d9e4f1e17757f06782f4820aa91154ca8b2
sepiiroth <37743946+sepiiroth@users.noreply.github.com>
2021-01-31 02:54:16 +0100

    mainmenu

---
+++ Scar/Assets/Scripts/MenuButtons.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButtons : MonoBehaviour
+{
+
+    public void DoExitGame() {
+        Application.Quit();
+    }
+
+    public void LoadScene(string level) {
+        SceneManager.LoadScene(level, LoadSceneMode.Single);
+    }
+
+    public void LoadOption() {
+        Debug.Log("Options!");
+    }
+}
+ 
