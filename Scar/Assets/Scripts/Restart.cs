e0bb2daaf85c0e7a5c683b9f8ae4f5d237784ae5
Ines <benaziz.ines@gmail.com>
2021-02-09 19:19:27 +0100

    creation village + pause

--- Scar/Assets/Scripts/Restart.cs
+++ Scar/Assets/Scripts/Restart.cs
@@ -6,8 +6,25 @@ using UnityEngine.SceneManagement;
 public class Restart : MonoBehaviour
 {
     public string scene;
+    public GameObject pause;
     public void Reload()
     {
         SceneManager.LoadScene(scene);
     }
+
+    public void Continue()
+    {
+        Time.timeScale = 1f;
+        pause.SetActive(false);
+    }
+
+    public void Update()
+    {
+        if(Input.GetKey(KeyCode.Escape))
+        {
+            Time.timeScale = 0f;
+            pause.SetActive(true);
+        }
+    }
+
 }
