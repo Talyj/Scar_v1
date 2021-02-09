e167cfa366a1c89cecec7e7ae93935968b9d78de
Nox <benaziz.ines@gmail.com>
2021-02-10 00:05:37 +0100

    accès donjon via village

---
+++ Scar/Assets/Scripts/LoadDonjon.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadDonjon : MonoBehaviour
+{
+    [SerializeField] private string donjon;
+    [SerializeField] private GameObject confirmation;
+    
+    void Start()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            confirmation.SetActive(true);
+        }
+    }
+
+    public void Confirme()
+    {
+        SceneManager.LoadScene(donjon);
+    }
+}
