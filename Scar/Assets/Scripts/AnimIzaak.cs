f74130d96d9c5c86374779738172256cda1303d9
Ines <benaziz.ines@gmail.com>
2021-03-30 19:45:39 +0200

    Ajout anim izaak

---
+++ Scar/Assets/Scripts/AnimIzaak.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class AnimIzaak : MonoBehaviour
+{
+    public Animator izaak;
+    
+    void Start()
+    {
+        izaak = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetAxis("Vertical") != 0 || Input.GetAxis("Horizontal") != 0)
+        {
+            izaak.SetBool("isRuning", true);
+        }
+    }
+}
