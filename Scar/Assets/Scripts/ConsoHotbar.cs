99f9fc8fbdd217fb9f0dc829d051c165407acdbb
sepiiroth <37743946+sepiiroth@users.noreply.github.com>
2021-04-04 13:03:24 +0200

    Hotbar utilisable

--- Scar/Assets/Scripts/ConsoHotbar.cs
+++ Scar/Assets/Scripts/ConsoHotbar.cs
@@ -6,18 +6,40 @@ using UnityEngine.UI;
 public class ConsoHotbar : MonoBehaviour
 {
     private GameObject player;
+    private GameObject amountBoard;
+    private AmountBoard amounts;
     [SerializeField] private SlotsInventaire hotBar;
 
     private void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
+        amountBoard = GameObject.FindWithTag("AmountBoard");
+        amounts = amountBoard.GetComponent<AmountBoard>();
     }
     public void Update()
     {
-        if(Input.GetKeyDown(KeyCode.F) && AmountBoard.amount_slot_hotbar > 0)
-        {
-            player.GetComponent<HealthPlayer>().currentHealth += player.GetComponent<HealthPlayer>().maxHealth * 0.6f;
-            AmountBoard.amount_slot_hotbar -= 1;
+        if(Input.GetKeyDown(KeyCode.F) && hotBar.isFull[0] == true) {
+            if(hotBar.slots[0].transform.GetChild(0).gameObject.name == "mana_potion_hotbar(Clone)") {
+                if(amounts.amount_slot_hotbar == 1) {
+                    //Mettre la ligne pour la MANA ici
+                    amounts.SetHotBarMany(-1);
+                    Destroy(hotBar.slots[0].transform.GetChild(0).gameObject);
+                    hotBar.isFull[0] = false;
+                } else if(amounts.amount_slot_hotbar > 1) {
+                    //Mettre la ligne pour la MANA ici
+                    amounts.SetHotBarMany(-1);
+                }
+            } else if(hotBar.slots[0].transform.GetChild(0).gameObject.name == "health_potion_hotbar(Clone)") {
+                if(amounts.amount_slot_hotbar == 1) {
+                    player.GetComponent<HealthPlayer>().currentHealth += player.GetComponent<HealthPlayer>().maxHealth * 0.6f;
+                    amounts.SetHotBarMany(-1);
+                    Destroy(hotBar.slots[0].transform.GetChild(0).gameObject);
+                    hotBar.isFull[0] = false;
+                } else if(amounts.amount_slot_hotbar > 1) {
+                    player.GetComponent<HealthPlayer>().currentHealth += player.GetComponent<HealthPlayer>().maxHealth * 0.6f;
+                    amounts.SetHotBarMany(-1);
+                }
+            }
         }
     }
 }
