801161123145a261200a3f6d2de7ebb08eb4ba1b
Talyj <julientay03@gmail.com>
2021-04-21 23:35:42 +0200

    same

---
+++ Scar/Assets/Scripts/Data.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataToSave
+{
+    public float shootDelay;
+    public float maxHealthPlayer;
+    public float rangeDamage;
+    public float closeDamage;
+    public int levelBoss;
+}
--- Scar/Assets/Scripts/InventaireExterne.cs
+++ Scar/Assets/Scripts/InventaireExterne.cs
@@ -30,9 +30,6 @@ public class InventaireExterne : MonoBehaviour
         string json = JsonUtility.ToJson(dataToSave);
         File.WriteAllText(destination, json);
         Debug.Log("Saved !");
-        
-        //DataToSave loadedData = JsonUtility.FromJson<DataToSave>(json);
-        //Debug.Log(loadedData.shootDelay);
     }
 
     private void Load()
@@ -44,50 +41,8 @@ public class InventaireExterne : MonoBehaviour
             Debug.Log("File Loaded !");
 
             DataToSave data = JsonUtility.FromJson<DataToSave>(saveString);
-            Debug.Log(data.rangeDamage);
+            GameInfo.rangedDamage = data.rangeDamage;
+            GameInfo.closedDamage = data.closeDamage;
         }
-    }
-    
-    private class DataToSave
-    {
-        public float shootDelay;
-        public float maxHealthPlayer;
-        public float rangeDamage;
-        public float closeDamage;
-        public int levelBoss;
-    }
-    
-    /*private void SaveTest()
-    {
-        string destination = Application.persistentDataPath + "/game.dat";
-        shootDelay = GunController.timeBetweenShots;
-        maxHealthPlayer = 200;
-        rangeDamage = GameInfo.rangedDamage;
-        closeDamage = GameInfo.closedDamage;
-        levelBoss = PlayerController.levelBoss;
-        
-        string[] data = new string[]
-        {
-            "shoot_delay=" + shootDelay,
-            "max_health=" + maxHealthPlayer,
-            "ranged_damage=" + rangeDamage,
-            "closed_damage=" + closeDamage,
-            "level_boss=" + levelBoss
-        };
-
-        string saveString = string.Join(SAVE_SEPARATOR, data);
-        File.WriteAllText(destination, saveString);
-        
-        Debug.Log("Saved !");
-    }*/
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
