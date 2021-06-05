10ff1a3a3a9fc40c261db8e12b97148846b134f3
sepiiroth <sepiiroth@gmail.com>
2021-06-05 18:29:46 +0200

    Résolution AmountBoard

--- Scar/Assets/Scripts/AmountBoard.cs
+++ Scar/Assets/Scripts/AmountBoard.cs
@@ -3,144 +3,161 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI; 
 using System.Linq;
+using System.IO;
 
 public class AmountBoard : MonoBehaviour {
 
     public int amount_piece = 0;
     public int amount_rubis = 0;
-    private int amount_slot_1 = 0;
-    private int amount_slot_2 = 0;
-    private int amount_slot_3 = 0;
-    private int amount_slot_hotbar = 0;
+    public int amount_slot_1 = 0;
+    public int amount_slot_2 = 0;
+    public int amount_slot_3 = 0;
+    public int amount_slot_card = 0;
+    public int amount_slot_hotbar = 0;
+    public string hotbar_type = "";
+    public string slot1_type = "";
+    public string slot2_type = "";
+    public string slot3_type = "";
+    public string slotcard_type = "";
+
     public GameObject amount_piece_object;
     public GameObject amount_rubis_object;
     public GameObject amount_slot_object_1;
     public GameObject amount_slot_object_2;
     public GameObject amount_slot_object_3;
     public GameObject amount_slot_hotbar_hotbar;
+    public GameObject first_part_inventory;
+    public GameObject fifth_part_inventory;
+    public GameObject fourth_part_hotbar;
+
     private SlotsInventaire slotsPiece;
     private SlotsInventaire slotsPotion;
     private SlotsInventaire slotsHotbar;
-    private GameObject itemPiece;
-    private GameObject itemRubis;
-    private GameObject itemPotion1;
-    private GameObject itemPotion2;
-    private GameObject itemPotion3;
-    private GameObject itemPotionHotbar;
-    private string hotbar_type;
+    public GameObject itemPiece;
+    public GameObject itemRubis;
+    public GameObject itemPotion1;
+    public GameObject itemPotion2;
+    public GameObject itemPotion3Shield;
+    public GameObject itemPotion3Destruct;
+    public GameObject itemPotion3Damage;
+    public GameObject itemPotion1Hotbar;
+    public GameObject itemPotion2Hotbar;
+    public GameObject itemPotion3Hotbar;
+    public GameObject itemPotion4Hotbar;
+    public GameObject itemPotion5Hotbar;
+    private string chemin, jsonString;
+
+    /*private string hotbar_type;
     private string slot3_type; 
     private string slot2_type;
-    private string slot1_type; 
+    private string slot1_type; */
 
     void Awake() {
-        SearchAllSlots();
-        slotsPiece = null;
+        chemin = Application.streamingAssetsPath + "/inventory.json";
+        jsonString = File.ReadAllText(chemin);
+        VariableForJSON inventaire = JsonUtility.FromJson<VariableForJSON>(jsonString);
+        amount_piece = inventaire.amount_piece;
+        amount_rubis = inventaire.amount_rubis;
+        amount_slot_1 = inventaire.amount_slot_1;
+        amount_slot_2 = inventaire.amount_slot_2;
+        amount_slot_3 = inventaire.amount_slot_3;
+        amount_slot_card = inventaire.amount_slot_card;
+        amount_slot_hotbar = inventaire.amount_slot_hotbar;
+        hotbar_type = inventaire.hotbar_type;
+        slot1_type = inventaire.slot1_type;
+        slot2_type = inventaire.slot2_type;
+        slot3_type = inventaire.slot3_type;
+        slotcard_type = inventaire.slotcard_type;
+
+        if(amount_slot_1 >= 10) {
+            amount_slot_1 = 10;
+        } 
+        if(amount_slot_2 >= 10) {
+            amount_slot_2 = 10;
+        } 
+        if(amount_slot_3 >= 5) {
+            amount_slot_3 = 5;
+        }
+        if(amount_slot_hotbar >= 5) {
+            amount_slot_hotbar = 5;
+        }
+
+        slotsPiece = fifth_part_inventory.GetComponent<SlotsInventaire>();
+        slotsPotion = first_part_inventory.GetComponent<SlotsInventaire>();
+        slotsHotbar = fourth_part_hotbar.GetComponent<SlotsInventaire>();
+        CheckInventory();
+        /*slotsPiece = null;
         slotsPotion = null;
         slotsHotbar = null;
-        itemPiece = null;
-        itemRubis = null;
-        itemPotion1 = null;
-        itemPotion2 = null;
-        itemPotion3 = null;
-        itemPotionHotbar = null;
+        */
     }
 
-    //*** Met à jour l'inventaire même en changeant la scène ***//
-    void Update() {
-        SearchAllSlots();
-        if(SearchAllSlots() == 1) {
-            if(slotsPiece.isFull[0] == false && amount_piece > 0) {
-                Instantiate(itemPiece, slotsPiece.slots[0].transform, false);
-                slotsPiece.isFull[0] = true;
-                var p = amount_piece_object.GetComponent<Text>();
-                p.text = amount_piece.ToString();
-                amount_piece_object.SetActive(true);
-            }
+    private void CheckInventory() {
+        if(slotsPiece.isFull[0] == false && amount_piece > 0) {
+            Instantiate(itemPiece, slotsPiece.slots[0].transform, false);
+            slotsPiece.isFull[0] = true;
+            var p = amount_piece_object.GetComponent<Text>();
+            p.text = amount_piece.ToString();
+            amount_piece_object.SetActive(true);
+        }
 
-            if(slotsPiece.isFull[2] == false && amount_rubis > 0) {
-                Instantiate(itemRubis, slotsPiece.slots[2].transform, false);
-                slotsPiece.isFull[2] = true;
-                var p = amount_rubis_object.GetComponent<Text>();
-                p.text = amount_rubis.ToString();
-                amount_rubis_object.SetActive(true);
-            }
+        if(slotsPiece.isFull[2] == false && amount_rubis > 0) {
+            Instantiate(itemRubis, slotsPiece.slots[2].transform, false);
+            slotsPiece.isFull[2] = true;
+            var p = amount_rubis_object.GetComponent<Text>();
+            p.text = amount_rubis.ToString();
+            amount_rubis_object.SetActive(true);
+        }
 
-            if(slotsPotion.isFull[0] == false && amount_slot_1 > 0) {
-                Instantiate(itemPotion1, slotsPotion.slots[0].transform, false);
-                slotsPotion.isFull[0] = true;
-                var p = amount_slot_object_1.GetComponent<Text>();
-                p.text = amount_slot_1.ToString();
-                amount_slot_object_1.SetActive(true);
-            }
+        if(slotsPotion.isFull[0] == false && amount_slot_1 > 0) {
+            Instantiate(itemPotion1, slotsPotion.slots[0].transform, false);
+            slotsPotion.isFull[0] = true;
+            var p = amount_slot_object_1.GetComponent<Text>();
+            p.text = amount_slot_1.ToString();
+            amount_slot_object_1.SetActive(true);
+        }
 
-            if(slotsPotion.isFull[1] == false && amount_slot_2 > 0) {
-                Instantiate(itemPotion2, slotsPotion.slots[1].transform, false);
-                slotsPotion.isFull[1] = true;
-                var p = amount_slot_object_2.GetComponent<Text>();
-                p.text = amount_slot_2.ToString();
-                amount_slot_object_2.SetActive(true);
-            }
+        if(slotsPotion.isFull[1] == false && amount_slot_2 > 0) {
+            Instantiate(itemPotion2, slotsPotion.slots[1].transform, false);
+            slotsPotion.isFull[1] = true;
+            var p = amount_slot_object_2.GetComponent<Text>();
+            p.text = amount_slot_2.ToString();
+            amount_slot_object_2.SetActive(true);
+        }
 
-            if(slotsPotion.isFull[2] == false && amount_slot_3 > 0) {
-                Instantiate(itemPotion3, slotsPotion.slots[2].transform, false);
-                slotsPotion.isFull[2] = true;
-                var p = amount_slot_object_3.GetComponent<Text>();
-                p.text = amount_slot_3.ToString();
-                amount_slot_object_3.SetActive(true);
+        if(slotsPotion.isFull[2] == false && amount_slot_3 > 0) {
+            if(slot3_type == "damage_potion") {
+                Instantiate(itemPotion3Damage, slotsPotion.slots[2].transform, false);
+            } else if(slot3_type == "destruct_potion") {
+                Instantiate(itemPotion3Destruct, slotsPotion.slots[2].transform, false);
+            } else if(slot3_type == "shield_potion") {
+                Instantiate(itemPotion3Shield, slotsPotion.slots[2].transform, false);
             }
+            slotsPotion.isFull[2] = true;
+            var p = amount_slot_object_3.GetComponent<Text>();
+            p.text = amount_slot_3.ToString();
+            amount_slot_object_3.SetActive(true);
+        }
 
-            if(slotsHotbar.isFull[0] == false && amount_slot_hotbar > 0) {
-                Instantiate(itemPotionHotbar, slotsHotbar.slots[0].transform, false);
-                slotsHotbar.isFull[0] = true;
-                var p = amount_slot_hotbar_hotbar.GetComponent<Text>();
-                p.text = amount_slot_hotbar.ToString();
-                amount_slot_hotbar_hotbar.SetActive(true);
+        if(slotsHotbar.isFull[0] == false && amount_slot_hotbar > 0) {
+            chemin = Application.streamingAssetsPath + "/inventory.json";
+            jsonString = File.ReadAllText(chemin);
+            VariableForJSON inventaire = JsonUtility.FromJson<VariableForJSON>(jsonString);
+            if(inventaire.hotbar_type == "heal_potion") {
+                Instantiate(itemPotion1Hotbar, slotsHotbar.slots[0].transform, false);
+            } else if(inventaire.hotbar_type == "mana_potion") {
+                Instantiate(itemPotion2Hotbar, slotsHotbar.slots[0].transform, false);
+            } else if(inventaire.hotbar_type == "shield_potion") {
+                Instantiate(itemPotion3Hotbar, slotsHotbar.slots[0].transform, false);
+            } else if(inventaire.hotbar_type == "destruct_potion") {
+                Instantiate(itemPotion4Hotbar, slotsHotbar.slots[0].transform, false);
+            } else if(inventaire.hotbar_type == "damage_potion") {
+                Instantiate(itemPotion5Hotbar, slotsHotbar.slots[0].transform, false);
             }
-        }
-    }
-    
-    //*** Permet de rechercher tout les slots importants ***//
-    private int SearchAllSlots() {
-        var objects = Resources.FindObjectsOfTypeAll<GameObject>().Where(obj => obj.name == "Fifth Part");
-        foreach(var item in objects) {
-            slotsPiece = item.GetComponent<SlotsInventaire>();
-        }
-        objects = Resources.FindObjectsOfTypeAll<GameObject>().Where(obj => obj.name == "First Part");
-        foreach(var item in objects) {
-            slotsPotion = item.GetComponent<SlotsInventaire>();
-        }
-        objects = Resources.FindObjectsOfTypeAll<GameObject>().Where(obj => obj.name == "Fourth Part HotBar");
-        foreach(var item in objects) {
-            slotsHotbar = item.GetComponent<SlotsInventaire>();
-        }
-        objects = Resources.FindObjectsOfTypeAll<GameObject>().Where(obj => obj.name == "Piece Amount");
-        foreach(var item in objects) {
-            amount_piece_object = item;
-        }
-        objects = Resources.FindObjectsOfTypeAll<GameObject>().Where(obj => obj.name == "Rubis Amount");
-        foreach(var item in objects) {
-            amount_rubis_object = item;
-        }
-        objects = Resources.FindObjectsOfTypeAll<GameObject>().Where(obj => obj.name == "Slot 1 Amount");
-        foreach(var item in objects) {
-            amount_slot_object_1 = item;
-        }
-        objects = Resources.FindObjectsOfTypeAll<GameObject>().Where(obj => obj.name == "Slot 2 Amount");
-        foreach(var item in objects) {
-            amount_slot_object_2 = item;
-        }
-        objects = Resources.FindObjectsOfTypeAll<GameObject>().Where(obj => obj.name == "Slot 3 Amount");
-        foreach(var item in objects) {
-            amount_slot_object_3 = item;
-        }
-        objects = Resources.FindObjectsOfTypeAll<GameObject>().Where(obj => obj.name == "HotBar Potion Amount");
-        foreach(var item in objects) {
-            amount_slot_hotbar_hotbar = item;
-        }
-        if(slotsPiece != null && slotsPotion != null && slotsHotbar != null && amount_piece_object != null && amount_rubis_object != null && amount_slot_object_1 != null && amount_slot_object_2 != null && amount_slot_object_3 != null && amount_slot_hotbar_hotbar != null) {
-            return 1;
-        } else {
-            return 0;
+            slotsHotbar.isFull[0] = true;
+            var p = amount_slot_hotbar_hotbar.GetComponent<Text>();
+            p.text = amount_slot_hotbar.ToString();
+            amount_slot_hotbar_hotbar.SetActive(true);
         }
     }
 
@@ -250,8 +267,7 @@ public class AmountBoard : MonoBehaviour {
         amount_slot_1 = amount_slot_1 + x;
         var p = amount_slot_object_1.GetComponent<Text>();
         p.text = amount_slot_1.ToString();
-        if(amount_slot_1 > 0 && slot.isFull[0] == false) {
-            itemPotion1 = item;
+        if(amount_slot_1 > 0 && amount_slot_1 < 10 && slot.isFull[0] == false) {
             Instantiate(itemPotion1, slot.slots[0].transform, false);
             slot.isFull[0] = true;
             amount_slot_object_1.SetActive(true);
@@ -268,8 +284,7 @@ public class AmountBoard : MonoBehaviour {
         amount_slot_2 = amount_slot_2 + x;
         var p = amount_slot_object_2.GetComponent<Text>();
         p.text = amount_slot_2.ToString();
-        if(amount_slot_2 > 0 && slot.isFull[1] == false) {
-            itemPotion2 = item;
+        if(amount_slot_2 > 0 && amount_slot_2 < 10 && slot.isFull[1] == false) {
             Instantiate(itemPotion2, slot.slots[1].transform, false);
             slot.isFull[1] = true;
             amount_slot_object_2.SetActive(true);
@@ -286,9 +301,8 @@ public class AmountBoard : MonoBehaviour {
         amount_slot_3 = amount_slot_3 + x;
         var p = amount_slot_object_3.GetComponent<Text>();
         p.text = amount_slot_3.ToString();
-        if(amount_slot_3 > 0 && slot.isFull[2] == false) {
-            itemPotion3 = item;
-            Instantiate(itemPotion3, slot.slots[2].transform, false);
+        if(amount_slot_3 > 0  && amount_slot_3 < 5 && slot.isFull[2] == false) {
+            Instantiate(item, slot.slots[2].transform, false);
             slot.isFull[2] = true;
             amount_slot_object_3.SetActive(true);
         } else if(amount_slot_3 == 0) {
@@ -325,10 +339,13 @@ public class AmountBoard : MonoBehaviour {
         var p = amount_slot_hotbar_hotbar.GetComponent<Text>();
         p.text = amount_slot_hotbar.ToString();
         if(amount_slot_hotbar >= 1) {
-            itemPotionHotbar = item;
             amount_slot_hotbar_hotbar.SetActive(true);
         } else if(amount_slot_hotbar == 0) {
             amount_slot_hotbar_hotbar.SetActive(false);
         }
+       //Debug.Log(slot.transform.gameObject.transform.GetChild(2).gameObject.transform.GetChild(slot.transform.gameObject.transform.GetChild(2).gameObject.transform.childCount-1).gameObject.tag);
+        //if(slot.transform.gameObject.transform.GetChild(2).gameObject.transform.GetChild(slot.transform.gameObject.transform.GetChild(2).gameObject.transform.childCount-1).gameObject.tag == "DamagePotionInventory" || slot.transform.gameObject.transform.GetChild(2).gameObject.transform.GetChild(slot.transform.gameObject.transform.GetChild(2).gameObject.transform.childCount-1).gameObject.tag == "DestructPotionInventory" || slot.transform.gameObject.transform.GetChild(2).gameObject.transform.GetChild(slot.transform.gameObject.transform.GetChild(2).gameObject.transform.childCount-1).gameObject.tag == "ShieldPotionInventory") {
+          //0  slot.transform.gameObject.transform.GetChild(2).gameObject.transform.GetChild(slot.transform.gameObject.transform.GetChild(2).gameObject.transform.childCount-1).gameObject.transform.SetSiblingIndex(0);
+        //}
     }
 }
--- Scar/Assets/Scripts/LoadDonjon.cs
+++ Scar/Assets/Scripts/LoadDonjon.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.IO;
 
 public class LoadDonjon : MonoBehaviour
 {
     [SerializeField] private GameObject confirmation;
+    [SerializeField] private AmountBoard amountBoard;
+    private string chemin, jsonString;
 
     private void OnTriggerEnter(Collider other)
     {
@@ -26,6 +29,24 @@ public class LoadDonjon : MonoBehaviour
 
     public void Confirme()
     {
+        chemin = Application.streamingAssetsPath + "/inventory.json";
+        jsonString = File.ReadAllText(chemin);
+        VariableForJSON inventaire = JsonUtility.FromJson<VariableForJSON>(jsonString);
+        inventaire.amount_piece = amountBoard.amount_piece;
+        inventaire.amount_rubis = amountBoard.amount_rubis;
+        inventaire.amount_slot_1 = amountBoard.amount_slot_1;
+        inventaire.amount_slot_2 = amountBoard.amount_slot_2;
+        inventaire.amount_slot_3 = amountBoard.amount_slot_3;
+        inventaire.amount_slot_card = amountBoard.amount_slot_card;
+        inventaire.amount_slot_hotbar = amountBoard.amount_slot_hotbar;
+        inventaire.hotbar_type = amountBoard.hotbar_type;
+        inventaire.slot1_type = amountBoard.slot1_type;
+        inventaire.slot2_type = amountBoard.slot2_type;
+        inventaire.slot3_type = amountBoard.slot3_type;
+        inventaire.slotcard_type = amountBoard.slotcard_type;
+        jsonString = JsonUtility.ToJson(inventaire);
+        File.WriteAllText(chemin, jsonString);
+
         string currentScene = SceneManager.GetActiveScene().name;
         switch (currentScene)
         {
