dc4d7885557e0bd4b2e9669d603c664c3353f730
Nox <benaziz.ines@gmail.com>
2021-06-06 11:04:47 +0200

    inte panels boss

---
+++ Scar/Assets/Scripts/UI/AnimBoss.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class AnimBoss : MonoBehaviour
+{
+    public GameObject bossPanel;
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Time.timeScale = 0f;
+            bossPanel.transform.Translate(100, 0, 0);
+            new WaitForSeconds(3);
+            bossPanel.transform.Translate(100, 0, 0);
+        }
+    }
+}
