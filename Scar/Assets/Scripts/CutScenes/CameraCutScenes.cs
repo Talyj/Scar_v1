f06a0566651ba4961d496ba0b5edfd4e4849905c
Talyj <julientay03@gmail.com>
2021-07-13 01:07:49 +0200

    Ajout de la caméra pas encore fonctionnelle mais bientôt

---
+++ Scar/Assets/Scripts/CutScenes/CameraCutScenes.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using TreeEditor;
+using UnityEngine;
+
+public class CameraCutScenes : MonoBehaviour
+{
+    
+    private Transform boss;
+    private int speedCamera = 6;
+    private int distance;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        CameraMovement();
+    }
+
+    private void CameraMovement()
+    {
+        boss = GameObject.FindGameObjectWithTag("boss").transform;
+        transform.LookAt(new Vector3(boss.position.x, transform.position.y, boss.position.z) - new Vector3(transform.position.x, transform.position.y, transform.position.z));
+        float dist = Vector3.Distance(boss.transform.position, gameObject.transform.position);
+        if (dist <= 10)
+        {
+            transform.RotateAround(boss.position, Vector3.up, 20 * Time.deltaTime * speedCamera);
+        }
+        else
+        {
+            transform.position += transform.forward * Time.deltaTime * speedCamera;   
+        }
+    }
+    
+}
---
+++ Scar/Assets/Scripts/CutScenes/CutScenesEnter.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutScenesEnter : MonoBehaviour
+{
+    public GameObject cameraPlayer;
+    public GameObject cutSceneCam;
+    private bool firstActive = true;
+    private float timer;
+
+    private GameObject boss;
+    private GameObject player;
+
+    private void Start()
+    {
+        boss = GameObject.FindGameObjectWithTag("boss");
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") & firstActive)
+        {
+            firstActive = false;
+            cutSceneCam.SetActive(true);
+            cameraPlayer.SetActive(false);
+            StartCoroutine(FinishCut());
+        }
+
+        IEnumerator FinishCut()
+        {
+            yield return new WaitForSeconds(2);
+            cameraPlayer.SetActive(true);
+            cutSceneCam.SetActive(false);
+        }
+        
+        IEnumerator ResumeAfterNSeconds(float timePeriod)
+        {
+            yield return new WaitForEndOfFrame();
+            timer += Time.unscaledDeltaTime;
+            if(timer < timePeriod)
+                StartCoroutine(ResumeAfterNSeconds(3.0f));
+            else
+            {
+                Time.timeScale = 1;                //Resume
+                timer = 0;
+            }
+        }
+    }
+}
